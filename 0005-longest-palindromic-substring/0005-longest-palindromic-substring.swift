9ed68a504d0c3b20a0495eb19e2725fad0e95efb
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-04-13 14:25:45 +0900

    Time: 3649 ms (9.16%), Space: 20.1 MB (63.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.swift
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.swift
@@ -1,33 +1,29 @@
 class Solution {
     func longestPalindrome(_ s: String) -> String {
-        let characters = Array(s)  // 빠른 인덱싱을 위해 문자 배열로 변환
-        var res = ""
-        var resLen = 0
-        var l: Int
-        var r: Int
-        for i in 0..<characters.count {
-            // odd Length
-            l = i; r = i
-            while l >= 0 && r < characters.count && characters[l] == characters[r] {
-                if (r - l + 1) > resLen {
-                    res = String(characters[l...r])
-                    resLen = r - l + 1
+        var arr = Array(s)
+        var left: Int, right: Int
+        var res: String = ""
+
+        for i in 0..<arr.count {
+            left = i; right = i
+            while left >= 0 && right < arr.count && arr[left] == arr[right] {
+                if right - left + 1 > res.count {
+                    res = String(arr[left...right])
                 }
-                l -= 1
-                r += 1
+                left -= 1
+                right += 1 
             }
 
-            // even Length
-            l = i; r = i + 1
-            while l >= 0 && r < characters.count && characters[l] == characters[r] {
-                if (r - l + 1) > resLen {
-                    res = String(characters[l...r])
-                    resLen = r - l + 1
+            left = i; right = i + 1
+            while left >= 0 && right < arr.count && arr[left] == arr[right] {
+                if right - left + 1 > res.count {
+                    res = String(arr[left...right])
                 }
-                l -= 1
-                r += 1
+                left -= 1
+                right += 1
             }
         }
+
         return res
     }
 }
