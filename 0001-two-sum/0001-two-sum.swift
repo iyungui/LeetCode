5764046b520adf74115ebd4201810ac72d263be4
Yung-ui <121233350+iyungui@users.noreply.github.com>
2025-05-08 16:47:54 +0900

    Time: 0 ms (100%), Space: 20.1 MB (25.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,13 +1,9 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
-
-        for (index, value) in nums.enumerated() {
-            if let addent = dict[value] {
-                return [addent, index]
-            } else {
-                dict[target - value] = index
-            }
+        for (idx, num) in nums.enumerated() {
+            if let addent = dict[num] { return [addent, idx] }
+            else { dict[target - num] = idx }
         }
         return []
     }
