1f67120c16198cda4f8181888fbf16d4c7c93090
iyungui <121233350+iyungui@users.noreply.github.com>
2025-07-29 22:42:31 +0900

    Time: 25 ms (12.5%), Space: 22.9 MB (34.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.swift
+++ 0217-contains-duplicate/0217-contains-duplicate.swift
@@ -2,9 +2,11 @@ class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var seen = Set<Int>()
 
-        for num in nums {
-            if !seen.insert(num).inserted { return true }
+        for num in nums {   // O(n) but can return early when it's the best case.
+            if seen.contains(num) { return true }   // O(1)
+            seen.insert(num)    // O(1)
         }
+
         return false
     }
 }
