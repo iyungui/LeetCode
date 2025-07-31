950f1b2a897678792f3d214c9ad178adc86c2924
iyungui <121233350+iyungui@users.noreply.github.com>
2025-07-31 11:49:27 +0900

    Time: 0 ms (100%), Space: 23.7 MB (13.24%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.swift
+++ 0338-counting-bits/0338-counting-bits.swift
@@ -1,5 +1,6 @@
 class Solution {
     func countBits(_ n: Int) -> [Int] {
+        if n == 0 { return [0] }
         var ans = [Int](repeating: 0, count: n + 1)
         for i in 1...n {
             ans[i] = ans[i >> 1] + (i & 1)
