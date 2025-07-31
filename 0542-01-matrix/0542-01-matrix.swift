2590453ca528eb30c7dfb12ab60dfdba5af70417
iyungui <121233350+iyungui@users.noreply.github.com>
2025-07-31 15:35:16 +0900

    Time: 11 ms (86.02%), Space: 20.7 MB (40.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.swift
+++ 0542-01-matrix/0542-01-matrix.swift
@@ -1,38 +1,38 @@
 class Solution {
     func updateMatrix(_ mat: [[Int]]) -> [[Int]] {
-        let m = mat[0].count
-        let n = mat.count
+        let dy = [-1, 0, 1, 0]
+        let dx = [0, 1, 0, -1]
+        let n = mat.count, m = mat[0].count
+        var dist: [[Int]] = Array(repeating: Array(repeating: -1, count: m), count: n)
 
-        var distance = Array(repeating: Array(repeating: -1, count: m), count: n)
-        var queue: [(Int, Int)] = []
+        var queue: [(y: Int, x: Int)] = []
+        var front = 0
+
         for i in 0..<n {
             for j in 0..<m {
                 if mat[i][j] == 0 {
-                    distance[i][j] = 0
+                    dist[i][j] = 0
                     queue.append((i, j))
                 }
             }
         }
 
-        let dy = [-1, 0, 1, 0]
-        let dx = [0, 1, 0, -1]
-
-        var index = 0
-        while index < queue.count {
-            let (y, x) = queue[index]
-            index += 1
+        while front < queue.count {
+            let (y, x) = queue[front]
+            
             for i in 0..<4 {
                 let ny = y + dy[i]
                 let nx = x + dx[i]
 
                 if ny < 0 || ny >= n || nx < 0 || nx >= m { continue }
-                if distance[ny][nx] != -1 { continue }
-
-                distance[ny][nx] = distance[y][x] + 1
+                if dist[ny][nx] != -1 { continue }
+                dist[ny][nx] = dist[y][x] + 1
                 queue.append((ny, nx))
             }
+
+            front += 1
         }
 
-        return distance
+        return dist
     }
 }
