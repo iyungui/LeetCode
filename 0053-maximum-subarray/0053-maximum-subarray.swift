df4ff1996e6036cf64a4d6784be3df757b5107ea
iyungui <121233350+iyungui@users.noreply.github.com>
2025-07-31 13:07:20 +0900

    Time: 4 ms (15.97%), Space: 20.9 MB (95.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.swift
+++ 0053-maximum-subarray/0053-maximum-subarray.swift
@@ -1,11 +1,14 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        var curSum = nums[0], maxSum = nums[0]
+        var curSum = nums[0]
+        var maxSum = nums[0]
 
         for i in 1..<nums.count {
-            curSum = max(nums[i], curSum + nums[i])
+            // decide whether 'extend the current subarray' or 'start a new subarray'
+            curSum = max(curSum + nums[i], nums[i])
             maxSum = max(curSum, maxSum)
         }
+
         return maxSum
     }
 }
