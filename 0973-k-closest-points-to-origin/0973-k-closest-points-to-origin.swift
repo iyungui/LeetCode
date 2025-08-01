b0a4bb1b92631db5568a0c829a6875e1e1fe5a18
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-01 16:55:31 +0900

    Time: 20 ms (54.41%), Space: 21.9 MB (15.69%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.swift
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.swift
@@ -1,23 +1,9 @@
 class Solution {
     func kClosest(_ points: [[Int]], _ k: Int) -> [[Int]] {
-        
-        var result: [[Int]] = []
-        var maxHeap: [(distance: Int, point: [Int])] = []
-        
-        for point in points {
-            let distance = point[0] * point[0] + point[1] * point[1]
-            
-            if maxHeap.count < k {
-                // 힙이 k개 미만이면 추가
-                maxHeap.append((distance: distance, point: point))
-                maxHeap.sort { $0.distance > $1.distance } // Max heap 유지
-            } else if distance < maxHeap[0].distance {
-                // 현재 점이 힙의 최대값보다 가까우면 교체
-                maxHeap[0] = (distance: distance, point: point)
-                maxHeap.sort { $0.distance > $1.distance } // Max heap 유지
-            }
-        }
-        
-        return maxHeap.map { $0.point }
+        return points.sorted { point1, point2 in
+            let dist1 = point1[0] * point1[0] + point1[1] * point1[1]
+            let dist2 = point2[0] * point2[0] + point2[1] * point2[1]
+            return dist1 < dist2
+        }.prefix(k).map { $0 }
     }
 }
