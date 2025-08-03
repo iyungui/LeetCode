dc2522603154d320427ac3a9a6ba04af9f5b41a3
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-03 18:38:24 +0900

    Time: 0 ms (100%), Space: 20.3 MB (34.62%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.swift
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var queue: [TreeNode] = [root]
+        var ret: [[Int]] = []
+        while queue.count > 0 {
+            var level: [Int] = []
+            var size = queue.count
+            for _ in 0..<size {
+                let node = queue.removeFirst()
+                level.append(node.val)
+                if let left = node.left { queue.append(left) }
+                if let right = node.right { queue.append(right) }
+            }
+            ret.append(level)
+        }
+
+        return ret
+    }
+}
