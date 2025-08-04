3eb565676805393d6d6ed83ede79d5cea1b94d75
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-04 14:07:46 +0900

    Time: 4 ms (20.48%), Space: 21.4 MB (6.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.swift
@@ -1,25 +1,16 @@
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
         var left = 0, right = 1
-        let n = prices.count
         var maxProfit = 0
-        while right < n {
-            if prices[left] <= prices[right] {
-                let diff = prices[right] - prices[left]
-                
-                // update profit
-                if maxProfit < diff {
-                    maxProfit = diff
-                }
-
+        while right <= prices.count - 1 {
+            let curProfit = prices[right] - prices[left]
+            if curProfit > 0 {
+                maxProfit = max(maxProfit, curProfit)
             } else {
-                // update pointer because it doesn't need to buy at higher day
                 left = right
             }
-
             right += 1
         }
-
         return maxProfit
     }
 }
