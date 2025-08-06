7fb6a0407cf7e82a751e262759a289ed3e8b0d2f
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-06 20:01:57 +0900

    Time: 37 ms (79.4%), Space: 18.3 MB (43.78%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.swift
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.swift
@@ -16,16 +16,19 @@ class Solution {
     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
         guard let root = root, let p = p, let q = q else { return nil }
 
-        var current = root
+        var cur = root
 
         while true {
-            if p.val < current.val && q.val < current.val {
-                current = current.left!
-            } else if p.val > current.val && q.val > current.val {
-                current = current.right!
-            } else {
-                return current
+            // p와 q가 모두 현재 노드보다 작으면 왼쪽으로
+            if p.val < cur.val && q.val < cur.val {
+                cur = cur.left!
             }
+            // p와 q가 모두 현재 노드보다 크다면 오른쪽으로
+            else if p.val > cur.val && q.val > cur.val {
+                cur = cur.right!
+            }
+            // 그렇지 않으면 현재 노드가 LCA
+            else { return cur }
         }
     }
 }
