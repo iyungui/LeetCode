f8dd77bcce79aad14db54e1f4b3c0d997112b6df
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-07 16:15:46 +0900

    Time: 0 ms (100%), Space: 19.7 MB (66.05%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.swift
+++ 0206-reverse-linked-list/0206-reverse-linked-list.swift
@@ -10,18 +10,15 @@
  */
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-        guard head != nil else { return nil }
-
-        var prev: ListNode? = nil
-        var cur: ListNode? = head
-
-        while cur != nil {
-            let next = cur!.next
-            cur!.next = prev
-            prev = cur
-            cur = next
+        guard let head = head, let next = head.next else {
+            return head
         }
 
-        return prev
+        let reversedHead = reverseList(next)
+
+        next.next = head
+        head.next = nil
+
+        return reversedHead
     }
 }
