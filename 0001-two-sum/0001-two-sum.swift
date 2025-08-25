5f8303d99b381abd9ed971cf899457c9fb24e258
iyungui <121233350+iyungui@users.noreply.github.com>
2025-08-25 18:15:20 +0900

    Time: 0 ms (100%), Space: 19.6 MB (86.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.swift
+++ 0001-two-sum/0001-two-sum.swift
@@ -1,13 +1,14 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        let sortedNums = nums.enumerated().sorted { $0.element < $1.element }
-        var l = 0, r = nums.count - 1
-        while l < r {
-            let sum = sortedNums[l].element + sortedNums[r].element
-            if sum == target { return [sortedNums[l].offset, sortedNums[r].offset] }
-            else if sum < target { l += 1 }
-            else { r -= 1 }
+        var dict = [Int: Int]()
+
+        for (idx, val) in nums.enumerated() {
+            if let addent = dict[val] {
+                return [addent, idx]
+            }
+            dict[target - val] = idx
         }
+
         return []
     }
 }
